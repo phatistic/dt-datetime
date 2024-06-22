7e5d3e958adbac74ffa5ee18f081698a8b3cd097
phatistic <150102276+phatistic@users.noreply.github.com>
2024-06-22 07:20:27 +0700

    Initial files for MakeCode project

---
+++ jacdac.ts
@@ -0,0 +1,17 @@
+// auto-generated, do not edit.
+namespace myModules {
+
+    //% fixedInstance whenUsed block="realTimeClock3"
+    export const realTimeClock3 = new modules.RealTimeClockClient("realTimeClock3");
+
+    //% fixedInstance whenUsed block="rotaryEncoder3"
+    export const rotaryEncoder3 = new modules.RotaryEncoderClient("rotaryEncoder3");
+
+    //% fixedInstance whenUsed block="dcCurrentMeasurement3"
+    export const dcCurrentMeasurement3 = new modules.DcCurrentMeasurementClient("dcCurrentMeasurement3");
+
+    //% fixedInstance whenUsed block="dcVoltageMeasurement3"
+    export const dcVoltageMeasurement3 = new modules.DcVoltageMeasurementClient("dcVoltageMeasurement3");
+
+}
+    
